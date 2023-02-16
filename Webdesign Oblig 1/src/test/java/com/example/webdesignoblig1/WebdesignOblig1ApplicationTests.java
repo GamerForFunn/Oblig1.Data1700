1fcef89511da6f8545af0ce16577abcb6d944aa5
GamerForFunn <114579022+GamerForFunn@users.noreply.github.com>
2023-02-16 10:48:28 +0100

    Uploaded code worked on before repository was made

---
+++ Webdesign Oblig 1/src/test/java/com/example/webdesignoblig1/WebdesignOblig1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webdesignoblig1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebdesignOblig1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ Webdesign Oblig 1/target/classes/static/controller.js
@@ -0,0 +1,6 @@
+function movieSelect (){
+    let display = document.getElementById("movieSelect1").value;
+    document.getElementById("selected").innerHTML = display;
+    console.log("It works")
+
+}
